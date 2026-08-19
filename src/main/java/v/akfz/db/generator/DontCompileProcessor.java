7c542982ba41e201583bf3a49fd867edce50d5e1
akaize <sahsa61842@gmail.com>
2026-08-19 05:47:30 +0300

    Stab version

--- src/main/java/v/akfz/db/generator/DontCompileProcessor.java
+++ src/main/java/v/akfz/db/generator/DontCompileProcessor.java
@@ -18,6 +18,7 @@ import com.sun.source.util.Trees;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 import com.sun.tools.javac.tree.TreeTranslator;
+import com.sun.tools.javac.util.ListBuffer;
 
 import v.akfz.db.annotation.DontCompile;
 
@@ -53,12 +54,27 @@ public class DontCompileProcessor extends AbstractProcessor {
 
             cu.accept(new TreeTranslator() {
                 @Override
-                public void visitClassDef(JCTree.JCClassDecl treeDecl) {
-                    if (treeDecl.equals(tree)) {
-                        this.result = null;
-                    } else {
-                        super.visitClassDef(treeDecl);
+                public void visitTopLevel(JCCompilationUnit topLevel) {
+                    ListBuffer<JCTree> newDefs = new ListBuffer<>();
+                    for (JCTree def : topLevel.defs) {
+                        if (!def.equals(tree)) {
+                            newDefs.append(def);
+                        }
                     }
+                    topLevel.defs = newDefs.toList();
+                    super.visitTopLevel(topLevel);
+                }
+
+                @Override
+                public void visitClassDef(JCTree.JCClassDecl classDecl) {
+                    ListBuffer<JCTree> newDefs = new ListBuffer<>();
+                    for (JCTree def : classDecl.defs) {
+                        if (!def.equals(tree)) {
+                            newDefs.append(def);
+                        }
+                    }
+                    classDecl.defs = newDefs.toList();
+                    super.visitClassDef(classDecl);
                 }
             });
         }
