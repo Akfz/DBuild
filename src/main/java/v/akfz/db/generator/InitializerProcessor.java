4b27742f2ac1a6f07e1d39fb99ad1804c6ac5fb5
akaize <sahsa61842@gmail.com>
2026-08-19 14:17:54 +0300

    Stab version

--- src/main/java/v/akfz/db/generator/InitializerProcessor.java
+++ src/main/java/v/akfz/db/generator/InitializerProcessor.java
@@ -71,12 +71,21 @@ public class InitializerProcessor extends AbstractProcessor {
             boolean loaderAllowsFabric = loader == null || loader == LoaderType.FabricLike || loader == LoaderType.Both;
             boolean loaderAllowsForge = loader == null || loader == LoaderType.ForgeLike || loader == LoaderType.Both;
 
+            boolean hasInit = hasInitMethod(typeElement);
+            if (!hasInit) {
+                processingEnv.getMessager().printMessage(
+                        Diagnostic.Kind.WARNING,
+                        "Class '" + typeElement.getQualifiedName() + "' is annotated with @GenerateInitializer, but missing 'public void init()' method!",
+                        typeElement
+                );
+            }
+
             if (isFabric && loaderAllowsFabric) {
-                generateFabricInitializer(filer, mainClass, isClient, endPackage, annotation.addClassNameFabric());
+                generateFabricInitializer(filer, mainClass, isClient, endPackage, annotation.addClassNameFabric(), hasInit);
             }
 
             if ((isForge || isNeoForge) && loaderAllowsForge) {
-                generateForgeInitializer(filer, mainClass, modId, isClient, endPackage, annotation.addClassNameForge(), isNeoForge);
+                generateForgeInitializer(filer, mainClass, modId, isClient, endPackage, annotation.addClassNameForge(), isNeoForge, hasInit);
             }
         }
         return true;
@@ -99,7 +108,7 @@ public class InitializerProcessor extends AbstractProcessor {
     }
 
     private void generateForgeInitializer(Filer filer, TypeName mainClass, String modId, boolean isClient,
-                                          String packageId, String addClassName, boolean isNeoForge) {
+                                          String packageId, String addClassName, boolean isNeoForge, boolean hasInit) {
 
         String generatedClassName = getSimpleName(mainClass) + addClassName;
         String fullClassName = packageId + "." + generatedClassName;
@@ -109,9 +118,6 @@ public class InitializerProcessor extends AbstractProcessor {
         String basePkg = isNeoForge ? "net.neoforged" : "net.minecraftforge";
 
         ClassName modAnnotation = ClassName.get(basePkg + ".fml.common", "Mod");
-        ClassName contextClass = ClassName.get(basePkg + ".fml.javafmlmod", "FMLJavaModLoadingContext");
-        ClassName setupEventClass = ClassName.get(basePkg + ".fml.event.lifecycle",
-                isClient ? "FMLClientSetupEvent" : "FMLCommonSetupEvent");
 
         FieldSpec mainInstanceField = FieldSpec.builder(mainClass, "MAININSTANCE", Modifier.PRIVATE, Modifier.STATIC, Modifier.FINAL)
                 .initializer("new $T()", mainClass)
@@ -123,31 +129,38 @@ public class InitializerProcessor extends AbstractProcessor {
                 .addStatement("return MAININSTANCE")
                 .build();
 
-        MethodSpec setupMethod = MethodSpec.methodBuilder("onSetup")
-                .addModifiers(Modifier.PRIVATE)
-                .addParameter(setupEventClass, "event")
-                .addStatement("MAININSTANCE.init()")
-                .build();
+        MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC);
 
-        MethodSpec constructor = MethodSpec.constructorBuilder()
-                .addModifiers(Modifier.PUBLIC)
-                .addStatement("$T.get().getModEventBus().addListener(this::onSetup)", contextClass)
-                .build();
-
-        TypeSpec generatedClass = TypeSpec.classBuilder(generatedClassName)
+        TypeSpec.Builder classBuilder = TypeSpec.classBuilder(generatedClassName)
                 .addModifiers(Modifier.PUBLIC)
                 .addAnnotation(AnnotationSpec.builder(modAnnotation).addMember("value", "$S", modId).build())
                 .addField(mainInstanceField)
-                .addMethod(getInstanceMethod)
-                .addMethod(constructor)
-                .addMethod(setupMethod)
-                .build();
+                .addMethod(getInstanceMethod);
+
+        if (hasInit) {
+            ClassName contextClass = ClassName.get(basePkg + ".fml.javafmlmod", "FMLJavaModLoadingContext");
+            ClassName setupEventClass = ClassName.get(basePkg + ".fml.event.lifecycle",
+                    isClient ? "FMLClientSetupEvent" : "FMLCommonSetupEvent");
+
+            MethodSpec setupMethod = MethodSpec.methodBuilder("onSetup")
+                    .addModifiers(Modifier.PRIVATE)
+                    .addParameter(setupEventClass, "event")
+                    .addStatement("MAININSTANCE.init()")
+                    .build();
+
+            constructorBuilder.addStatement("$T.get().getModEventBus().addListener(this::onSetup)", contextClass);
+            classBuilder.addMethod(setupMethod);
+        }
+
+        classBuilder.addMethod(constructorBuilder.build());
+        TypeSpec generatedClass = classBuilder.build();
 
         writeClass(filer, packageId, generatedClassName, generatedClass, "Forge");
     }
 
     private void generateFabricInitializer(Filer filer, TypeName mainClass, boolean isClient,
-                                           String packageId, String addClassName) {
+                                           String packageId, String addClassName, boolean hasInit) {
 
         String generatedClassName = getSimpleName(mainClass) + addClassName;
         String fullClassName = packageId + "." + generatedClassName;
@@ -167,19 +180,21 @@ public class InitializerProcessor extends AbstractProcessor {
                 .addStatement("return MAININSTANCE")
                 .build();
 
-        MethodSpec initMethod = MethodSpec.methodBuilder(methodName)
+        MethodSpec.Builder initMethodBuilder = MethodSpec.methodBuilder(methodName)
                 .addAnnotation(Override.class)
                 .addModifiers(Modifier.PUBLIC)
-                .returns(void.class)
-                .addStatement("MAININSTANCE.init()")
-                .build();
+                .returns(void.class);
+
+        if (hasInit) {
+            initMethodBuilder.addStatement("MAININSTANCE.init()");
+        }
 
         TypeSpec generatedClass = TypeSpec.classBuilder(generatedClassName)
                 .addModifiers(Modifier.PUBLIC)
                 .addSuperinterface(fabricInterface)
                 .addField(mainInstanceField)
                 .addMethod(getInstanceMethod)
-                .addMethod(initMethod)
+                .addMethod(initMethodBuilder.build())
                 .build();
 
         writeClass(filer, packageId, generatedClassName, generatedClass, "Fabric");
@@ -203,5 +218,19 @@ public class InitializerProcessor extends AbstractProcessor {
         }
         String name = typeName.toString();
         return name.substring(name.lastIndexOf('.') + 1);
+    }
+
+    private boolean hasInitMethod(TypeElement typeElement) {
+        for (Element enclosed : typeElement.getEnclosedElements()) {
+            if (enclosed.getKind() == javax.lang.model.element.ElementKind.METHOD
+                    && enclosed.getSimpleName().contentEquals("init")) {
+
+                javax.lang.model.element.ExecutableElement method = (javax.lang.model.element.ExecutableElement) enclosed;
+                if (method.getParameters().isEmpty() && method.getModifiers().contains(Modifier.PUBLIC)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
