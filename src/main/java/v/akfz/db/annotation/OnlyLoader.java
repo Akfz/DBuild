13be0b6ed1f7d220201413bfd46cbd5d1f68b40c
akaize <sahsa61842@gmail.com>
2026-09-24 23:54:49 +0300

    fix javadoc

--- src/main/java/v/akfz/db/annotation/OnlyLoader.java
+++ src/main/java/v/akfz/db/annotation/OnlyLoader.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/** Compiles & packs this class only for the listed loaders. Requires dbuild plugin's dontCompile(). */
+/** Compiles and packs this class only for the listed loaders. Requires dbuild plugin's dontCompile(). */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 public @interface OnlyLoader {
